d77a5007bb0d2bddace42fc1140864cc4cabf53d
ccobb73 <che_cobb@yahoo.com>
2018-03-23 11:17:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,47 @@
-var currentStaticCacheName = 'restaurant-static-v1';
+self.addEventListener('install', function(event) {
+  // Perform install steps
+});
+var CACHE_NAME = 'restaurant-cache';
+var urlsToCache = [
+  '/index.html',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/dbhelper.js',
+  'css/styles.css',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
+];
 
 self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(currentStaticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'js/dbhelper.js',
-        'css/styles.css',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
-      ]);
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames
-          .filter(function(cacheName) {
-            return (
-              cacheName.startsWith('restaurant-') && cacheName != currentStaticCacheName
-            );
-          })
-          .map(function(cacheName) {
-            return caches.delete(cacheName);
-          })
-      );
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {
+      ignoreSearch: true
+    }).then(response => {
+      return response || fetch(event.request);
     })
+    .catch(err => console.log(err, event.request))
   );
 });
